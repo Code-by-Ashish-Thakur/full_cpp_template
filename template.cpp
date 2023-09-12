b93fe07bb50d8914370f2ceb10e773b20f9e83ed
oder_1 <110951020+Code-by-Ashish-Thakur@users.noreply.github.com>
2023-09-12 18:30:33 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -260,6 +260,57 @@ ll MIN4(ll a, ll b, ll c, ll d)
     return min(a, min(b, min(c, d)));
 }
 
+
+/************************MERGE SORT********************************/
+void merge(int A[], int mid, int low, int high)
+{
+    int i, j, k, B[100005];
+    i = low;
+    j = mid + 1;
+    k = low;
+    while (i <= mid && j <= high)
+    {
+        if (A[i] < A[j])
+        {
+            B[k] = A[i];
+            i++;
+            k++;
+        }
+        else
+        {
+            B[k] = A[j];
+            j++;
+            k++;
+        }
+    }
+    while (i <= mid)
+    {
+        B[k] = A[i];
+        k++;
+        i++;
+    }
+    while (j <= high)
+    {
+        B[k] = A[j];
+        k++;
+        j++;
+    }
+    for (int it = low; it <= high; it++)
+    {
+        A[it] = B[it];
+    }
+    
+}
+void mergeSort(int A[], int low, int high){
+    int mid; 
+    if(low<high){
+        mid = (low + high) /2;
+        mergeSort(A, low, mid);
+        mergeSort(A, mid+1, high);
+        merge(A, mid, low, high);
+    }
+}
+
 //  **********************DFS******************************
 void DFS(int v, vector<int> adj[], vector<bool> &vis)
 {
